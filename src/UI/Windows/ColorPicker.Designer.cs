fd2d302f487bdeb0362d16233b26de01651121c4
Thomas Nind <TZNind@dundee.ac.uk>
2022-04-14 09:25:50 +0100

    Fixed trailing whitespace and RadioGroup sizes in ColorPicker

--- src/UI/Windows/ColorPicker.Designer.cs
+++ src/UI/Windows/ColorPicker.Designer.cs
@@ -31,119 +31,122 @@ namespace TerminalGuiDesigner {
         private Terminal.Gui.Button btnCancel;
         
         private void InitializeComponent() {
-            this.Text = "";
             this.Width = 50;
             this.Height = 20;
             this.X = Pos.Center();
             this.Y = Pos.Center();
-            this.TextAlignment = TextAlignment.Left;
+            this.Text = "";
+            this.Border.BorderStyle = Terminal.Gui.BorderStyle.Single;
+            this.Border.BorderBrush = Terminal.Gui.Color.Blue;
+            this.Border.Effect3D = true;
+            this.TextAlignment = Terminal.Gui.TextAlignment.Left;
             this.Title = "Color Picker";
             this.lblForeground = new Terminal.Gui.Label();
-            this.lblForeground.Data = "lblForeground";
-            this.lblForeground.Text = "Foreground:";
             this.lblForeground.Width = 11;
             this.lblForeground.Height = 1;
             this.lblForeground.X = 1;
             this.lblForeground.Y = 0;
-            this.lblForeground.TextAlignment = TextAlignment.Left;
+            this.lblForeground.Data = "lblForeground";
+            this.lblForeground.Text = "Foreground:";
+            this.lblForeground.TextAlignment = Terminal.Gui.TextAlignment.Left;
             this.Add(this.lblForeground);
             this.lblBackground = new Terminal.Gui.Label();
-            this.lblBackground.Data = "lblBackground";
-            this.lblBackground.Text = "Background:";
             this.lblBackground.Width = 11;
             this.lblBackground.Height = 1;
             this.lblBackground.X = 17;
             this.lblBackground.Y = 0;
-            this.lblBackground.TextAlignment = TextAlignment.Left;
+            this.lblBackground.Data = "lblBackground";
+            this.lblBackground.Text = "Background:";
+            this.lblBackground.TextAlignment = Terminal.Gui.TextAlignment.Left;
             this.Add(this.lblBackground);
             this.lblResult = new Terminal.Gui.Label();
-            this.lblResult.Data = "lblResult";
-            this.lblResult.Text = "Result:";
             this.lblResult.Width = 7;
             this.lblResult.Height = 1;
             this.lblResult.X = 38;
             this.lblResult.Y = 0;
-            this.lblResult.TextAlignment = TextAlignment.Left;
+            this.lblResult.Data = "lblResult";
+            this.lblResult.Text = "Result:";
+            this.lblResult.TextAlignment = Terminal.Gui.TextAlignment.Left;
             this.Add(this.lblResult);
             this.radiogroup1 = new Terminal.Gui.RadioGroup();
-            this.radiogroup1.Data = "radiogroup1";
-            this.radiogroup1.Text = "";
             this.radiogroup1.Width = 17;
             this.radiogroup1.Height = 16;
             this.radiogroup1.X = 3;
             this.radiogroup1.Y = 1;
-            this.radiogroup1.TextAlignment = TextAlignment.Left;
+            this.radiogroup1.Data = "radiogroup1";
+            this.radiogroup1.Text = "";
+            this.radiogroup1.TextAlignment = Terminal.Gui.TextAlignment.Left;
             this.radiogroup1.RadioLabels = new NStack.ustring[] {
-                    "Black ",
-                    "Blue ",
-                    "Green ",
-                    "Cyan ",
-                    "Red ",
-                    "Magenta ",
-                    "Brown ",
-                    "Gray ",
-                    "DarkGray ",
-                    "BrightBlue ",
-                    "BrightGreen ",
-                    "BrightCyan ",
-                    "BrightRed ",
-                    "BrightMagenta ",
-                    "BrightYellow ",
+                    "Black",
+                    "Blue",
+                    "Green",
+                    "Cyan",
+                    "Red",
+                    "Magenta",
+                    "Brown",
+                    "Gray",
+                    "DarkGray",
+                    "BrightBlue",
+                    "BrightGreen",
+                    "BrightCyan",
+                    "BrightRed",
+                    "BrightMagenta",
+                    "BrightYellow",
                     "White"};
             this.Add(this.radiogroup1);
             this.radiogroup2 = new Terminal.Gui.RadioGroup();
-            this.radiogroup2.Data = "radiogroup2";
-            this.radiogroup2.Text = "";
             this.radiogroup2.Width = 17;
             this.radiogroup2.Height = 16;
             this.radiogroup2.X = 19;
             this.radiogroup2.Y = 1;
-            this.radiogroup2.TextAlignment = TextAlignment.Left;
+            this.radiogroup2.Data = "radiogroup2";
+            this.radiogroup2.Text = "";
+            this.radiogroup2.TextAlignment = Terminal.Gui.TextAlignment.Left;
             this.radiogroup2.RadioLabels = new NStack.ustring[] {
-                    "Black ",
-                    "Blue ",
-                    "Green ",
-                    "Cyan ",
-                    "Red ",
-                    "Magenta ",
-                    "Brown ",
-                    "Gray ",
-                    "DarkGray ",
-                    "BrightBlue ",
-                    "BrightGreen ",
-                    "BrightCyan ",
-                    "BrightRed ",
-                    "BrightMagenta ",
-                    "BrightYellow ",
+                    "Black",
+                    "Blue",
+                    "Green",
+                    "Cyan",
+                    "Red",
+                    "Magenta",
+                    "Brown",
+                    "Gray",
+                    "DarkGray",
+                    "BrightBlue",
+                    "BrightGreen",
+                    "BrightCyan",
+                    "BrightRed",
+                    "BrightMagenta",
+                    "BrightYellow",
                     "White"};
             this.Add(this.radiogroup2);
             this.lblPreview = new Terminal.Gui.Label();
-            this.lblPreview.Data = "lblPreview";
-            this.lblPreview.Text = "\"Sample Text\"";
             this.lblPreview.Width = 13;
             this.lblPreview.Height = 1;
             this.lblPreview.X = 35;
             this.lblPreview.Y = 2;
-            this.lblPreview.TextAlignment = TextAlignment.Left;
+            this.lblPreview.Data = "lblPreview";
+            this.lblPreview.Text = "\"Sample Text\"";
+            this.lblPreview.TextAlignment = Terminal.Gui.TextAlignment.Left;
             this.Add(this.lblPreview);
             this.btnOk = new Terminal.Gui.Button();
-            this.btnOk.Data = "btnOk";
-            this.btnOk.Text = "Ok";
             this.btnOk.Width = 8;
             this.btnOk.Height = 1;
             this.btnOk.X = 10;
             this.btnOk.Y = 17;
-            this.btnOk.TextAlignment = TextAlignment.Centered;
+            this.btnOk.Data = "btnOk";
+            this.btnOk.Text = "Ok";
+            this.btnOk.TextAlignment = Terminal.Gui.TextAlignment.Centered;
             this.btnOk.IsDefault = true;
             this.Add(this.btnOk);
             this.btnCancel = new Terminal.Gui.Button();
-            this.btnCancel.Data = "btnCancel";
-            this.btnCancel.Text = "Cancel";
             this.btnCancel.Width = 10;
             this.btnCancel.Height = 1;
             this.btnCancel.X = 28;
             this.btnCancel.Y = 17;
-            this.btnCancel.TextAlignment = TextAlignment.Centered;
+            this.btnCancel.Data = "btnCancel";
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.TextAlignment = Terminal.Gui.TextAlignment.Centered;
             this.btnCancel.IsDefault = false;
             this.Add(this.btnCancel);
         }
