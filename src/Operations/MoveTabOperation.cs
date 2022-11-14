5b2679b778748598cf1a6cf767109f0eed4f5ef4
tznind <tznind@dundee.ac.uk>
2022-11-14 09:40:45 +0000

    Added MoveTabOperationTests

--- src/Operations/MoveTabOperation.cs
+++ src/Operations/MoveTabOperation.cs
@@ -2,16 +2,27 @@
 
 namespace TerminalGuiDesigner.Operations;
 
+/// <summary>
+/// Moves a <see cref="TabView.Tab"/> left or right within the ordering
+/// of tabs in a <see cref="TabView"/>.
+/// </summary>
 public class MoveTabOperation : TabViewOperation
 {
-    public int Adjustment { get; }
+    /// <summary>
+    /// Gets the number of index positions the <see cref="TabView.Tab"/> will
+    /// be moved. Negative for left, positive for right.
+    /// </summary>
+    private readonly int adjustment;
+    private readonly int originalIdx;
+    private readonly int newIndex;
 
     /// <summary>
+    /// Initializes a new instance of the <see cref="MoveTabOperation"/> class.
     /// Creates an operation that will change the ordering of tabs within
     /// a <see cref="TabView"/>.
     /// </summary>
-    /// <param name="design"></param>
-    /// <param name="adjustment">Negative to move tab left, positive to move tab right</param>
+    /// <param name="design">Wrapper for a <see cref="TabView"/>.</param>
+    /// <param name="adjustment">Negative to move tab left, positive to move tab right.</param>
     public MoveTabOperation(Design design, int adjustment)
         : base(design)
     {
@@ -20,28 +31,40 @@ public class MoveTabOperation : TabViewOperation
             this.IsImpossible = true;
         }
 
+        this.originalIdx = this.View.Tabs.ToList().IndexOf(this.SelectedTab);
+
+        if (this.originalIdx == -1)
+        {
+            this.IsImpossible = true;
+            return;
+        }
+
+        // calculate new index without falling off array
+        this.newIndex = Math.Max(0, Math.Min(this.View.Tabs.Count - 1, this.originalIdx + adjustment));
+
         // they are moving it nowhere?!
-        if (adjustment == 0)
+        if (this.newIndex == this.originalIdx)
         {
             this.IsImpossible = true;
         }
 
-        this.Adjustment = adjustment;
+        this.adjustment = adjustment;
     }
 
+    /// <inheritdoc/>
     public override string ToString()
     {
-        if (this.Adjustment == 0 || this.SelectedTab == null)
+        if (this.adjustment == 0 || this.SelectedTab == null)
         {
             return $"Bad Command '{this.GetType().Name}'";
         }
 
-        if (this.Adjustment < 0)
+        if (this.adjustment < 0)
         {
             return $"Move '{this.SelectedTab.Text}' Left";
         }
 
-        if (this.Adjustment > 0)
+        if (this.adjustment > 0)
         {
             return $"Move '{this.SelectedTab.Text}' Right";
         }
@@ -49,39 +72,32 @@ public class MoveTabOperation : TabViewOperation
         return base.ToString();
     }
 
-    protected override bool DoImpl()
-    {
-        return this.ApplyAdjustment(this.Adjustment);
-    }
-
+    /// <inheritdoc/>
     public override void Redo()
     {
         this.Do();
     }
 
+    /// <inheritdoc/>
     public override void Undo()
     {
-        this.ApplyAdjustment(-this.Adjustment);
+        if (this.SelectedTab == null)
+        {
+            return;
+        }
+
+        this.View.InsertTab(this.originalIdx, this.SelectedTab);
     }
 
-    private bool ApplyAdjustment(int adjustment)
+    /// <inheritdoc/>
+    protected override bool DoImpl()
     {
-        var originalIdx = this.View.Tabs.ToList().IndexOf(this.SelectedTab);
-
-        if (this.SelectedTab == null || originalIdx == -1)
+        if (this.SelectedTab == null)
         {
             return false;
         }
 
-        var newIndex = Math.Max(0, Math.Min(this.View.Tabs.Count - 1, originalIdx + adjustment));
-
-        // if we would end up putting it back where it was then abandon this operation
-        if (originalIdx == newIndex)
-        {
-            return false;
-        }
-
-        this.View.InsertTab(newIndex, this.SelectedTab);
+        this.View.InsertTab(this.newIndex, this.SelectedTab);
         return true;
     }
 }
---
+++ tests/Operations/MoveTabOperationTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using Terminal.Gui;
+using TerminalGuiDesigner;
+using TerminalGuiDesigner.Operations;
+
+namespace UnitTests.Operations
+{
+    internal class MoveTabOperationTests : Tests
+    {
+        [Test]
+        public void TestMoveTab_WrongViewType_Throws()
+        {
+            // Label is not a TabView so should get Exception
+            RoundTrip<Window, Label>((d, v) =>
+            {
+                Assert.Throws<ArgumentException>(()=>new MoveTabOperation(d, 1));
+            }, out _);
+        }
+
+        [Test]
+        public void TestMoveTab_OnlyOneTab_IsImpossible()
+        {
+            RoundTrip<Window, TabView>((d, v) =>
+            {
+                Assert.AreEqual(2, v.Tabs.Count, $"Expected {nameof(ViewFactory)} to create a {nameof(TabView)} with 2 example placeholder tabs");
+                v.RemoveTab(v.Tabs.ElementAt(1));
+                Assert.AreEqual(1, v.Tabs.Count);
+
+                Assert.IsTrue(new MoveTabOperation(d,1).IsImpossible,"Should be impossible to move tab when there is only one of them");
+            }, out _);
+        }
+
+
+
+        [TestCase(3,-8,0,true)] // move left lots
+        [TestCase(3, 55, 5, true)] // move right lots
+        [TestCase(0, 0, 0, false)] // move 0 nowhere
+        [TestCase(0, 1, 1, true)] // move 0 right 1
+        [TestCase(1, -1, 0, true)] // move 1 left 1
+        public void TestMoveTab_DoUndo(int idxToMove, int adjustment, int expectedNewIndex, bool expectPossible)
+        {
+            RoundTrip<Window, TabView>((d, v) =>
+            {
+                new AddTabOperation(d, "NewTab").Do();
+                new AddTabOperation(d, "NewTab").Do();
+                new AddTabOperation(d, "NewTab").Do();
+                new AddTabOperation(d, "NewTab").Do();
+
+                var toMove = v.Tabs.ElementAt(idxToMove);
+                var originalIndex = v.Tabs.IndexOf(toMove);
+
+                v.SelectedTab = toMove;
+                var op = new MoveTabOperation(d, adjustment);
+
+                if(expectPossible)
+                {
+                    Assert.IsFalse(op.IsImpossible);
+                    Assert.IsTrue(op.Do());
+                }
+                else
+                {
+                    Assert.IsTrue(op.IsImpossible);
+                    Assert.False(op.Do());
+                }
+
+                Assert.AreEqual(expectedNewIndex, v.Tabs.IndexOf(toMove));
+
+                op.Undo();
+                Assert.AreEqual(originalIndex, v.Tabs.IndexOf(toMove));
+
+                op.Redo();
+                Assert.AreEqual(expectedNewIndex, v.Tabs.IndexOf(toMove));
+
+            }, out _);
+        }
+    }
+}
