98f166acbef67805ec4cad6efcb84505946c188e
tznind <tznind@dundee.ac.uk>
2022-11-14 08:43:53 +0000

    Added MoveMenuItemRightOperationTests

--- src/Operations/MoveMenuItemRightOperation.cs
+++ src/Operations/MoveMenuItemRightOperation.cs
@@ -2,19 +2,57 @@ using Terminal.Gui;
 
 namespace TerminalGuiDesigner.Operations;
 
+/// <summary>
+/// Moves a <see cref="MenuItem"/> into a sub-menu of the <see cref="MenuItem"/>
+/// above it.  If it is the first item to be moved then it results in the creation
+/// of a new sub-menu.  In Terminal.Gui this means converting the above <see cref="MenuItem"/>
+/// into a <see cref="MenuBarItem"/> (the class for menu items that contain sub-menu items).
+/// </summary>
 public class MoveMenuItemRightOperation : MenuItemOperation
 {
     /// <summary>
-    /// Set to insert at a specific index.  Leave null
-    /// to simply move it to the bottom of the new submenu
+    /// Initializes a new instance of the <see cref="MoveMenuItemRightOperation"/> class.
     /// </summary>
-    public int? InsertionIndex { get; internal set; }
-
+    /// <param name="toMove">Moves the <paramref name="toMove"/> to the sub-menu of the <see cref="MenuItem"/> above it.</param>
     public MoveMenuItemRightOperation(MenuItem toMove)
         : base(toMove)
     {
+        if (this.Parent?.GetChildrenIndex(toMove) == 0)
+        {
+            this.IsImpossible = true;
+        }
     }
 
+    /// <summary>
+    /// Gets or Sets insertion at a specific index within the destination
+    /// sub-menu.  Leave null to simply move it to the bottom of the new
+    /// sub-menu.
+    /// </summary>
+    public int? InsertionIndex { get; set; }
+
+    /// <inheritdoc/>
+    public override void Redo()
+    {
+        if (this.OperateOn == null)
+        {
+            return;
+        }
+
+        new MoveMenuItemRightOperation(this.OperateOn).Do();
+    }
+
+    /// <inheritdoc/>
+    public override void Undo()
+    {
+        if (this.Parent == null || this.OperateOn == null)
+        {
+            return;
+        }
+
+        new MoveMenuItemLeftOperation(this.OperateOn).Do();
+    }
+
+    /// <inheritdoc/>
     protected override bool DoImpl()
     {
         if (this.Parent == null || this.OperateOn == null)
@@ -60,21 +98,6 @@ public class MoveMenuItemRightOperation : MenuItemOperation
         this.Bar?.SetNeedsDisplay();
 
         return true;
-    }
-
-    public override void Redo()
-    {
-        // TODO
-    }
-
-    public override void Undo()
-    {
-        if (this.Parent == null || this.OperateOn == null)
-        {
-            return;
-        }
-
-        new MoveMenuItemLeftOperation(this.OperateOn).Do();
     }
 
     private MenuBarItem ConvertToMenuBarItem(List<MenuItem> children, int idx)
---
+++ tests/Operations/MoveMenuItemRightOperationTests.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui;
+using TerminalGuiDesigner.Operations;
+
+namespace UnitTests.Operations
+{
+    internal class MoveMenuItemRightOperationTests : Tests
+    {
+        [Test]
+        public void TestMoveMenuItemRightOperation_ImpossibleIfSolo()
+        {
+            RoundTrip<Toplevel, MenuBar>((d, v) =>
+            {
+                var op = new MoveMenuItemRightOperation(v.Menus[0].Children[0]);
+                Assert.True(op.IsImpossible, "Expected it to be impossible to move first menu item to submenu when there is nothing above it");
+
+            }, out _);
+        }
+
+        [Test]
+        public void TestMoveMenuItemRightOperation_MoveToSubmenu()
+        {
+            RoundTrip<Toplevel, MenuBar>((d, v) =>
+            {
+                new AddMenuItemOperation(v.Menus[0].Children[0]).Do();
+
+
+                Assert.IsTrue(
+                    new MoveMenuItemRightOperation(v.Menus[0].Children[0]).IsImpossible,
+                    "Expected you still not to be able to move index 0 (because there is nothing above it)");
+
+                var toMove = v.Menus[0].Children[1];
+                var op = new MoveMenuItemRightOperation(toMove);
+                Assert.IsFalse(op.IsImpossible);
+
+                Assert.AreEqual(2, v.Menus[0].Children.Length);
+                Assert.IsInstanceOf<MenuItem>(v.Menus[0].Children[0]);
+                op.Do();
+
+                Assert.AreEqual(1, v.Menus[0].Children.Length);
+                Assert.IsInstanceOf<MenuBarItem>(v.Menus[0].Children[0],"Expected top entry to be converted to the Type that has sub items");
+                Assert.Contains(toMove,((MenuBarItem)v.Menus[0].Children[0]).Children);
+
+            }, out _);
+        }
+
+
+        [Test]
+        public void TestMoveMenuItemRightOperation_UndoRedo_RememberShortcut()
+        {
+            RoundTrip<Toplevel, MenuBar>((d, v) =>
+            {
+                new AddMenuItemOperation(v.Menus[0].Children[0]).Do();
+                var toMove = v.Menus[0].Children[1];
+
+                v.Menus[0].Children[0].Data = "yarg";
+                v.Menus[0].Children[0].Shortcut = Key.CtrlMask | Key.Y;
+                v.Menus[0].Children[1].Data = "blarg";
+                v.Menus[0].Children[1].Shortcut = Key.CtrlMask | Key.B;
+
+                // Move blarg to sub-menu of yarg
+                var op = new MoveMenuItemRightOperation(toMove);
+                op.Do();
+
+                Assert.AreEqual("yarg", v.Menus[0].Children[0].Data);
+                Assert.AreEqual(Key.CtrlMask | Key.Y, v.Menus[0].Children[0].Shortcut);
+                Assert.AreEqual("blarg", ((MenuBarItem)v.Menus[0].Children[0]).Children[0].Data);
+                Assert.AreEqual(Key.CtrlMask | Key.B, ((MenuBarItem)v.Menus[0].Children[0]).Children[0].Shortcut);
+
+                op.Undo();
+                Assert.AreEqual("yarg", v.Menus[0].Children[0].Data);
+                Assert.AreEqual(Key.CtrlMask | Key.Y, v.Menus[0].Children[0].Shortcut);
+                Assert.AreEqual("blarg", v.Menus[0].Children[1].Data);
+                Assert.AreEqual(Key.CtrlMask | Key.B, v.Menus[0].Children[1].Shortcut);
+
+                op.Redo();
+                Assert.AreEqual("yarg", v.Menus[0].Children[0].Data);
+                Assert.AreEqual(Key.CtrlMask | Key.Y, v.Menus[0].Children[0].Shortcut);
+                Assert.AreEqual("blarg", ((MenuBarItem)v.Menus[0].Children[0]).Children[0].Data);
+                Assert.AreEqual(Key.CtrlMask | Key.B, ((MenuBarItem)v.Menus[0].Children[0]).Children[0].Shortcut);
+
+            }, out _);
+        }
+    }
+}
