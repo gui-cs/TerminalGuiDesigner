e637dc123cd3694fd66809d228ba5a5b7a8db0a5
Brandon Thetford <github@snapsinazfs.com>
2023-12-14 15:17:45 -0700

    Finish with this test method

--- tests/EditorTests.cs
+++ tests/EditorTests.cs
@@ -13,14 +13,15 @@ namespace UnitTests;
 internal class EditorTests : Tests
 {
     [Test]
-    public void TestHasUnsavedChanges()
+    public void TestHasUnsavedChanges( )
     {
-        Editor e = new Editor();
+        //TODO: This test should be re-factored to test only HasUnsavedChanges
+        Editor e = new Editor( );
         Assume.That( e, Is.Not.Null.And.InstanceOf<Editor>( ) );
 
-        Assert.That( e.HasUnsavedChanges(), Is.False, "With nothing open there should not be any unsaved changes" );
+        Assert.That( e.HasUnsavedChanges( ), Is.False, "With nothing open there should not be any unsaved changes" );
 
-        DummyOperation dummyOperation1 = new ();
+        DummyOperation dummyOperation1 = new( );
         Assume.That( dummyOperation1, Is.Not.Null.And.InstanceOf<DummyOperation>( ) );
         Assume.That( dummyOperation1.IsImpossible, Is.False );
 
@@ -28,23 +29,29 @@ internal class EditorTests : Tests
         //Unsaved change tracking is dependent on calling Do on the OperationManager and will not work if called on the operation itself.
         //See https://github.com/dodexahedron/TerminalGuiDesigner/issues/34
         bool dummyOperation1Succeeded = false;
-        Assert.That( ( ) => dummyOperation1Succeeded = OperationManager.Instance.Do(dummyOperation1), Throws.Nothing );
+        Assert.That( ( ) => dummyOperation1Succeeded = OperationManager.Instance.Do( dummyOperation1 ), Throws.Nothing );
         Assert.That( dummyOperation1Succeeded );
 
-        Assert.That(e.HasUnsavedChanges(), "We have performed an operation and not yet saved");
+        Assert.That( e.HasUnsavedChanges( ), "We have performed an operation and not yet saved" );
 
         // fake a save
         IOperation? saveMock = null;
         Assert.That( ( ) => saveMock = OperationManager.Instance.GetLastAppliedOperation( ), Throws.Nothing );
-        Assert.That( saveMock, Is.Not.Null, "Expected DummyOperation to be known as the last performed" );
+        Assert.That( saveMock, Is.Not.Null.And.InstanceOf<DummyOperation>( ), "Expected DummyOperation to be known as the last performed" );
 
         e.LastSavedOperation = saveMock!.UniqueIdentifier;
-        Assert.That(e.HasUnsavedChanges(), Is.False, "Now that we have saved there should be no unsaved changes");
+        Assert.That( e.HasUnsavedChanges( ), Is.False, "Now that we have saved there should be no unsaved changes" );
 
-        OperationManager.Instance.Do(new DummyOperation());
-        Assert.That(e.HasUnsavedChanges(), "When we perform an operation after saving we now have changes again");
+        DummyOperation dummyOperation2 = new( );
+        Assume.That( dummyOperation2, Is.Not.Null.And.InstanceOf<DummyOperation>( ) );
+        Assume.That( dummyOperation2.IsImpossible, Is.False );
 
-        OperationManager.Instance.Undo();
+        bool dummyOperation2Succeeded = false;
+        Assert.That( ( ) => dummyOperation2Succeeded = OperationManager.Instance.Do( dummyOperation2 ), Throws.Nothing );
+        Assert.That( dummyOperation2Succeeded );
+        Assert.That( e.HasUnsavedChanges( ), "When we perform an operation after saving we now have changes again" );
+
+        Assert.That( ( ) => OperationManager.Instance.Undo( ), Throws.Nothing );
         Assert.That( e.HasUnsavedChanges( ), Is.False, "Undoing the newly performed operation should mean that we are back where we were when we saved (i.e. no changes)" );
     }
 
