3608ad1780115c68927c42aa7b1b4f0dee414a71
Brandon Thetford <github@snapsinazfs.com>
2023-12-14 15:30:29 -0700

    Rename to match class and convert the test method

---
+++ tests/ObjectExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using TerminalGuiDesigner;
+
+namespace UnitTests;
+
+[TestFixture]
+[TestOf( typeof( ObjectExtensions ) )]
+[Category( "Core" )]
+[Parallelizable( ParallelScope.All )]
+internal class ObjectExtensionsTests
+{
+    [Test]
+    public void String_CastToReflected( )
+    {
+        const string str = "cat";
+        object? obj = null;
+
+        Assert.That( ( ) => obj = str.CastToReflected( typeof( object ) ), Throws.Nothing );
+
+        //TODO: This is always going to return true if the previous assertion passed, because every object is an object.
+        Assert.That( obj, Is.Not.Null.And.InstanceOf<object>( ) );
+    }
+}
