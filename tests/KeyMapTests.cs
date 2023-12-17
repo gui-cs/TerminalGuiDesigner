705cd5c92ce6c9103a776afa3c19e9fed938a6f9
Brandon Thetford <github@snapsinazfs.com>
2023-12-16 21:28:54 -0700

    Re-link .editorconfig and ensure line endings consistent for test file

--- tests/KeyMapTests.cs
+++ tests/KeyMapTests.cs
@@ -1,7 +1,5 @@
-using System;
 using System.IO;
 using TerminalGuiDesigner.UI;
-using YamlDotNet.Serialization;
 
 namespace UnitTests;
 
@@ -12,20 +10,66 @@ namespace UnitTests;
 [Parallelizable( ParallelScope.All )]
 internal class KeyMapTests : Tests
 {
+    private const string _expectedKeysYamlContent =
+        @"EditProperties: F4
+ShowContextMenu: Enter
+ViewSpecificOperations: F4, ShiftMask
+EditRootProperties: F5
+ShowHelp: F1
+New: N, CtrlMask
+Open: O, CtrlMask
+Save: S, CtrlMask
+Redo: Y, CtrlMask
+Undo: Z, CtrlMask
+Delete: DeleteChar
+ToggleDragging: F3
+AddView: F2
+ToggleShowFocused: L, CtrlMask
+ToggleShowBorders: B, CtrlMask
+RightClick: Button3Clicked
+Copy: C, CtrlMask
+Paste: V, CtrlMask
+Rename: R, CtrlMask
+SetShortcut: T, CtrlMask
+SelectAll: A, CtrlMask
+MoveRight: CursorRight, ShiftMask
+MoveLeft: CursorLeft, ShiftMask
+MoveUp: CursorUp, ShiftMask
+MoveDown: CursorDown, ShiftMask
+ShowColorSchemes: F6
+SelectionColor:
+  NormalForeground: BrightGreen
+  NormalBackground: Green
+  HotNormalForeground: BrightGreen
+  HotNormalBackground: Green
+  FocusForeground: BrightYellow
+  FocusBackground: Green
+  HotFocusForeground: BrightYellow
+  HotFocusBackground: Green
+  DisabledForeground: BrightGreen
+  DisabledBackground: Green
+    
+";
+
     [Test]
-    public void TestSerializingKeyMap()
+    [Category( "Change Control" )]
+    [Description( "Ensures the default keymap file is present and as expected for testing, with allowances for line ending changes." )]
+    [Order( 1 )]
+    public void DefaultKeyMapFile_NotChanged( )
     {
-        var keys = Path.Combine(TestContext.CurrentContext.TestDirectory, "Keys.yaml");
-        FileAssert.Exists(keys);
+        // This may seem redundant to just having it all in code, but this is a two-way guarantee,
+        // to ensure that changes to the file OR the collection in code are deliberate.
+        var defaultKeyMapFileName = Path.Combine( TestContext.CurrentContext.TestDirectory, "Keys.yaml" );
 
-        var km = new KeyMap();
+        Assert.That( defaultKeyMapFileName, Does.Exist );
+        Assert.That( defaultKeyMapFileName, Is.Not.Empty );
 
-        var serializer = new Serializer();
-        var expected = serializer.Serialize(km);
+        FileInfo defaultKeyMapFileInfo = new( defaultKeyMapFileName );
 
-        ClassicAssert.AreEqual(
-            expected.Replace("\r\n", "\n").Trim(),
-            File.ReadAllText(keys).Replace("\r\n", "\n").Trim(),
-            $"The default yaml file ('Keys.yaml') that ships with TerminalGuiDesigner should match the default values in KeyMap. Set it to:{Environment.NewLine}{expected}");
+        Assert.That( defaultKeyMapFileInfo, Has.Length.EqualTo( 921 ) );
+
+        Assert.That( File.ReadAllText( defaultKeyMapFileName ),
+                     Is.EqualTo( _expectedKeysYamlContent ),
+                     "Content of Keys.yaml, including newline style, must match expected input." );
     }
 }
