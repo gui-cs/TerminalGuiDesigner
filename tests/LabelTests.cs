ee2cebd44a07a5ca69c26c7a1d7544d73a49d391
Brandon Thetford <github@snapsinazfs.com>
2023-12-20 23:34:44 -0700

    Initial examples of a standardized way to test all supported types
    Can specify view types, properties, and expected types of the properties
    Easy to add, either in-line as at the bottom, or via delegating to another IEnumerable, as just above that, for organizational purposes.

---
+++ tests/LabelTests.cs
@@ -0,0 +1,15 @@
+﻿namespace UnitTests;
+
+    internal class LabelTests
+    {
+        internal static IEnumerable<TestCaseData> ExpectedDesignableProperties_Cases
+        {
+            get
+            {
+                yield return new( ViewFactory.Create<Label>( 11, 1, "Hello World" ), "X", typeof( Pos ) );
+                yield return new( ViewFactory.Create<Label>( 11, 1, "Hello World" ), "Y", typeof( Pos ) );
+                yield return new( ViewFactory.Create<Label>( 11, 1, "Hello World" ), "Text", typeof( string ) );
+            }
+        }
+    }
+
