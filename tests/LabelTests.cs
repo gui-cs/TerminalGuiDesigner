678d1ec8a70f416b7b9581cf4c10d6535f6f0f9a
Brandon Thetford <github@snapsinazfs.com>
2023-12-20 23:04:55 -0700

    This is essentially the same test.

--- tests/LabelTests.cs
+++ tests/LabelTests.cs
@@ -66,44 +66,4 @@ internal class LabelTests : Tests
 
         Assert.That( lblDesign.View.X.ToString( ), Is.EqualTo( $"Factor({0.5})" ) );
     }
-
-    [Test]
-    public void Test_ChangingLabelX_PosDesigner()
-    {
-        var file = new FileInfo("Test_ChangingLabelX.cs");
-        var viewToCode = new ViewToCode();
-        var designOut = viewToCode.GenerateNewView(file, "YourNamespace", typeof(Window));
-
-        var op = new AddViewOperation(new Label("Hello World"), designOut, "myLabel");
-        op.Do();
-
-        // the Hello world label
-        var lblDesign = designOut.GetAllDesigns().Single(d => d.View is Label);
-        var xProp = lblDesign.GetDesignableProperties().Single(p => p.PropertyInfo.Name.Equals("X"));
-
-        lblDesign.View.IsInitialized = true;
-
-        OperationManager.Instance.Do(new SetPropertyOperation(lblDesign, xProp, xProp.GetValue(), Pos.At(10)));
-
-        var percent50 = Pos.Percent(50);
-        var percent30 = Pos.Percent(30);
-
-        OperationManager.Instance.Do(new SetPropertyOperation(lblDesign, xProp, xProp.GetValue(), percent50));
-        OperationManager.Instance.Do(new SetPropertyOperation(lblDesign, xProp, xProp.GetValue(), Pos.At(10)));
-        OperationManager.Instance.Do(new SetPropertyOperation(lblDesign, xProp, xProp.GetValue(), percent30));
-        OperationManager.Instance.Do(new SetPropertyOperation(lblDesign, xProp, xProp.GetValue(), percent50));
-        OperationManager.Instance.Do(new SetPropertyOperation(lblDesign, xProp, xProp.GetValue(), Pos.At(10)));
-
-        OperationManager.Instance.Undo();
-        OperationManager.Instance.Redo();
-        OperationManager.Instance.Undo();
-        OperationManager.Instance.Redo();
-        OperationManager.Instance.Undo();
-        OperationManager.Instance.Undo();
-        OperationManager.Instance.Redo();
-        OperationManager.Instance.Redo();
-        OperationManager.Instance.Undo();
-
-        ClassicAssert.AreEqual($"Factor({0.5})", lblDesign.View.X.ToString());
-    }
 }
