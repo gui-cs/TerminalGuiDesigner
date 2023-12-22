860f070a6254ffa7ab0a71a92366f9dda055bd2f
Brandon Thetford <github@snapsinazfs.com>
2023-12-21 22:57:15 -0700

    (WIP)Add two important tests
    Tests that ensure ViewFactory.Create behavior is the same for both generic and non-generic forms.
    Currently WIP - just syncing for visibility
    Tests pass, but with intentionally-thrown warnings about things that still need to be explicitly tested for a better guarantee that certain types come back the same

--- tests/ViewFactoryTests.cs
+++ tests/ViewFactoryTests.cs
@@ -1,33 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
 using System.Reflection;
-using Terminal.Gui;
-using TerminalGuiDesigner;
+using System.Runtime.Serialization;
 
 namespace UnitTests;
 
 [TestFixture]
 [TestOf( typeof( ViewFactory ) )]
 [Category( "Core" )]
+[Order( 1 )]
 internal class ViewFactoryTests : Tests
 {
-    private static Type[] KnownUnsupportedTypes_ExpectedTypes( )
+    /// <summary>
+    ///   Gets every known supported <see cref="View" /> type as an uninitialized object of the corresponding type for testing of generic methods.
+    /// </summary>
+    private static IEnumerable<TestCaseData> Create_And_CreateT_Type_Provider
     {
-        return new[]
-        {
-            typeof( Toplevel ),
-            typeof( Dialog ),
-            typeof( FileDialog ),
-            typeof( SaveDialog ),
-            typeof( OpenDialog ),
-            typeof( ScrollBarView ),
-            typeof( TreeView<> ),
-            typeof( Slider<> ),
-            typeof( Frame ),
-            typeof( Wizard ),
-            typeof( WizardStep )
-        };
+        get { return ViewFactory_SupportedViewTypes.Select( t => new TestCaseData( FormatterServices.GetUninitializedObject( t ) ) ); }
     }
 
     private static MenuBarItem[] ViewFactory_DefaultMenuBarItems => ViewFactory.DefaultMenuBarItems;
@@ -35,6 +23,109 @@ internal class ViewFactoryTests : Tests
     private static Type[] ViewFactory_KnownUnsupportedTypes => ViewFactory.KnownUnsupportedTypes;
 
     private static IEnumerable<Type> ViewFactory_SupportedViewTypes => ViewFactory.SupportedViewTypes;
+
+    [Test]
+    [TestCaseSource( nameof( Create_And_CreateT_Type_Provider ) )]
+    [Category( "Change Control" )]
+    [Description( "This test makes sure that both the generic and non-generic Create methods return objects with the same property values" )]
+#pragma warning disable CS0618 // Type or member is obsolete
+    public void Create_And_CreateT_ReturnEquivalentInstancesForSameInputs<T>( T dummyInvalidObject )
+        where T : View, new( )
+    {
+        T? createdByNonGeneric = ViewFactory.Create( typeof( T ) ) as T;
+        Assume.That( createdByNonGeneric, Is.Not.Null.And.InstanceOf<T>( ) );
+
+        T createdByGeneric = ViewFactory.Create<T>( );
+        Assume.That( createdByGeneric, Is.Not.Null.And.InstanceOf<T>( ) );
+
+        PropertyInfo[] publicPropertiesOfType = typeof( T ).GetProperties( BindingFlags.Instance | BindingFlags.Public ).Where( p => p.CanRead ).ToArray( );
+        Assert.Multiple( ( ) =>
+        {
+            foreach ( PropertyInfo property in publicPropertiesOfType )
+            {
+                object? nonGenericPropertyValue = property.GetValue( createdByNonGeneric );
+                object? genericPropertyValue = property.GetValue( createdByGeneric );
+
+                // First, if they're both null, we're good so just skip it.
+                if ( nonGenericPropertyValue is null && genericPropertyValue is null )
+                {
+                    continue;
+                }
+
+                // If one or the other isn't null, assert they both are not null
+                Assert.Multiple( ( ) =>
+                {
+                    Assert.That( nonGenericPropertyValue, Is.Not.Null );
+                    Assert.That( genericPropertyValue, Is.Not.Null );
+                } );
+
+                // Special cases for certain properties by property type, property name, and/or tested view type.
+                // In general, we could actually skip basically everything that isn't explicitly in Create,
+                // but doing it this way allows us to just test everything and only skip what we absolutely have to.
+                switch ( dummyInvalidOject: dummyInvalidObject, property )
+                {
+                    case (_, not null) when property.PropertyType == typeof( Frame ):
+                        Assert.That( nonGenericPropertyValue!.ToString( ), Is.EqualTo( genericPropertyValue!.ToString( ) ) );
+                        continue;
+                    case (_, not null) when property.PropertyType.IsAssignableTo( typeof( Dim ) ):
+                        Assert.That( (Dim)nonGenericPropertyValue!, Is.EqualTo( (Dim)genericPropertyValue! ) );
+                        continue;
+                    case (_, not null) when property.PropertyType.IsAssignableTo( typeof( TextFormatter ) ):
+                        TextFormatter nonGenericTextFormatter = (TextFormatter)nonGenericPropertyValue!;
+                        TextFormatter genericTextFormatter = (TextFormatter)genericPropertyValue!;
+                        Assert.That( nonGenericTextFormatter.ToCodePrimitiveExpression( ), Is.EqualTo( genericTextFormatter.ToCodePrimitiveExpression( ) ) );
+                        continue;
+                    case (_, { Name: "TabIndexes" }):
+                        var nonGenericTabIndexes = (ReadOnlyCollection<View>)nonGenericPropertyValue!;
+                        var genericTabIndexes = (ReadOnlyCollection<View>)genericPropertyValue!;
+                        Assert.That(
+                            nonGenericTabIndexes,
+                            Is.EquivalentTo( genericTabIndexes )
+                              .Using<View, View>( CompareTwoViews ) );
+                        continue;
+                    case (ComboBox, { Name: "Subviews" }):
+                    case (ScrollView, { Name: "Subviews" }):
+                    case (MenuBar, { Name: "Menus" }):
+                    case (TableView, { Name: "Table" }):
+                    case (TabView, { Name: "Tabs" }):
+                    case (TabView, { Name: "SelectedTab" }):
+                    case (TabView, { Name: "Subviews" }):
+                    case (TileView, { Name: "Subviews" }):
+                    case (TileView, { Name: "Tiles" }):
+                        // Warn about these, until they are implemented (WIP)
+                        Assert.Warn( $"{property.Name} not yet checked on {typeof( T ).Name}" );
+                        continue;
+                    case (_, { Name: "ContextMenu" }):
+                        // Safe to skip these, as we don't set them in Create
+                        continue;
+                }
+
+                Assert.That(
+                    nonGenericPropertyValue,
+                    Is.EqualTo( genericPropertyValue ),
+                    $"Property {property!.Name} of type {property.ReflectedType!.Name} mismatch. Generic: {genericPropertyValue} Non-Generic: {nonGenericPropertyValue}" );
+            }
+        } );
+    }
+#pragma warning restore CS0618 // Type or member is obsolete
+
+    [Test]
+    [TestCaseSource( nameof( Create_And_CreateT_Type_Provider ) )]
+    [Category( "Change Control" )]
+    [Description( "This test makes sure that both the generic and non-generic Create methods return non-null instances of the same type" )]
+#pragma warning disable CS0618 // Type or member is obsolete
+#pragma warning disable IDE0060 // Remove unused parameter
+    public void Create_And_CreateT_ReturnExpectedTypeForSameInputs<T>( T dummyTypeForNUnit )
+        where T : View, new( )
+    {
+        T? createdByNonGeneric = ViewFactory.Create( typeof( T ) ) as T;
+        Assert.That( createdByNonGeneric, Is.Not.Null.And.InstanceOf<T>( ) );
+
+        T createdByGeneric = ViewFactory.Create<T>( );
+        Assert.That( createdByGeneric, Is.Not.Null.And.InstanceOf<T>( ) );
+    }
+#pragma warning restore IDE0060 // Remove unused parameter
+#pragma warning restore CS0618 // Type or member is obsolete
 
     [Test]
     [RequiresThread]
@@ -90,7 +181,7 @@ internal class ViewFactoryTests : Tests
         MethodInfo viewFactoryCreateTConcrete = viewFactoryCreateTGeneric.MakeGenericMethod( unsupportedType );
 
         object? createdView = null;
-        object?[] methodParameters = Enumerable.Repeat<object?>( null, viewFactoryCreateTConcrete.GetParameters(  ).Length).ToArray( );
+        object?[] methodParameters = Enumerable.Repeat<object?>( null, viewFactoryCreateTConcrete.GetParameters( ).Length ).ToArray( );
 
         Assert.That( ( ) => createdView = viewFactoryCreateTConcrete.Invoke( null, methodParameters ),
                      Throws.TypeOf<TargetInvocationException>( ).With.InnerException.TypeOf<NotSupportedException>( ) );
@@ -139,9 +230,33 @@ internal class ViewFactoryTests : Tests
         Assert.That( KnownUnsupportedTypes_ExpectedTypes, Contains.Item( typeDeclaredUnsupportedInViewFactory ) );
     }
 
+    private bool CompareTwoViews( View nonGenericTabIndex, View genericTabIndex )
+    {
+        Assert.Warn( "View comparison only done by bounds check." );
+        return nonGenericTabIndex.Bounds == genericTabIndex.Bounds;
+    }
+
     private static IEnumerable<Type> CreateT_ThrowsOnUnsupportedTypes_Cases( )
     {
         // Filtering generics out because they'll still throw, but a different exception
         return ViewFactory_KnownUnsupportedTypes.Where( t => !t.IsGenericType );
+    }
+
+    private static Type[] KnownUnsupportedTypes_ExpectedTypes( )
+    {
+        return new[]
+        {
+            typeof( Toplevel ),
+            typeof( Dialog ),
+            typeof( FileDialog ),
+            typeof( SaveDialog ),
+            typeof( OpenDialog ),
+            typeof( ScrollBarView ),
+            typeof( TreeView<> ),
+            typeof( Slider<> ),
+            typeof( Frame ),
+            typeof( Wizard ),
+            typeof( WizardStep )
+        };
     }
 }
