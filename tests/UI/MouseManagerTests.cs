68fc50401843d3f68bc1bd6ed8f469e508556d1f
Brandon Thetford <github@snapsinazfs.com>
2024-01-03 01:02:20 -0700

    Re-sort after name changes

--- tests/UI/MouseManagerTests.cs
+++ tests/UI/MouseManagerTests.cs
@@ -72,7 +72,6 @@ internal class MouseManagerTests : Tests
             Assert.That( OperationManager.Instance.UndoStackSize, Is.Zero );
         } );
 
-
         // user releases mouse (in place)
         e = new( )
         {
@@ -91,6 +90,66 @@ internal class MouseManagerTests : Tests
             // we have now committed the drag so could undo
             Assert.That( OperationManager.Instance.UndoStackSize, Is.EqualTo( 1 ) );
         } );
+    }
+
+    [Test]
+    public void DragResizeView_CannotResize_1By1View( [Values( 0, 3 )] int locationOfViewX, [Values( 0, 3 )] int locationOfViewY )
+    {
+        Design d = Get10By10View( );
+
+        View view = ViewFactory.Create<View>( );
+        view.Width = Dim.Fill( );
+        view.X = locationOfViewX;
+        view.Y = locationOfViewY;
+        view.Width = 1;
+        view.Height = 1;
+
+        Design design = new( d.SourceCode, "myView", view );
+        view.Data = design;
+        d.View.Add( view );
+
+        d.View.LayoutSubviews( );
+
+        MouseManager mgr = new( );
+
+        // we haven't done anything yet
+        Assert.That( OperationManager.Instance.UndoStackSize, Is.Zero );
+        Assert.That( view.Frame.X, Is.EqualTo( locationOfViewX ) );
+        Assert.That( view.Frame.Y, Is.EqualTo( locationOfViewY ) );
+        Assert.That( view.Bounds.Width, Is.EqualTo( 1 ) );
+        Assert.That( view.Bounds.Height, Is.EqualTo( 1 ) );
+
+        // user presses down in the lower right of control
+        MouseEvent e = new( )
+        {
+            X = locationOfViewX,
+            Y = locationOfViewY,
+            Flags = MouseFlags.Button1Pressed
+        };
+
+        View? hit = view.HitTest( e, out bool isBorder, out bool isLowerRight );
+        Assert.That( hit, Is.SameAs( view ) );
+        Assert.That( isBorder );
+        Assert.That( isLowerRight, Is.False, "Upper left should never be considered lower right even if view is 1x1" );
+
+        mgr.HandleMouse( e, d );
+
+        Assert.That( view.Frame.Y, Is.EqualTo( locationOfViewY ) );
+
+        // we still haven't committed to anything
+        Assert.That( OperationManager.Instance.UndoStackSize, Is.Zero );
+
+        // user pulled view size down and left
+        e = new( )
+        {
+            X = 6,
+            Y = 3,
+            Flags = MouseFlags.Button1Pressed
+        };
+        mgr.HandleMouse( e, d );
+
+        Assert.That( view.Bounds.Width, Is.EqualTo( 1 ) );
+        Assert.That( view.Bounds.Height, Is.EqualTo( 1 ) );
     }
 
     [Test]
@@ -189,6 +248,82 @@ internal class MouseManagerTests : Tests
             Assert.That( view.Width, Is.EqualTo( Dim.Fill( ) ) );
             Assert.That( view.Height, Is.EqualTo( Dim.Sized( 1 ) ) );
         } );
+    }
+
+    [TestCase( 1, 1, 4, 6, new[] { 0, 2 } )]      // drag from 1,1 to 4,6 and expect labels 0 and 2 to be selected
+    [TestCase( 1, 1, 10, 10, new[] { 0, 1, 2 } )] // drag over all
+    public void DragSelectionBox( int xStart, int yStart, int xEnd, int yEnd, int[] expectSelected )
+    {
+        Design d = Get10By10View( );
+
+        /*
+          Hi
+            Hi
+          Hi
+        */
+
+        Label lbl1 = new( 2, 1, "Hi" );
+        Label lbl2 = new( 4, 2, "Hi" );
+        Label lbl3 = new( 2, 3, "Hi" );
+
+        Design lbl1Design = new( d.SourceCode, "lbl1", lbl1 );
+        Design lbl2Design = new( d.SourceCode, "lbl2", lbl2 );
+        Design lbl3Design = new( d.SourceCode, "lbl3", lbl3 );
+
+        lbl1.Data = lbl1Design;
+        lbl2.Data = lbl2Design;
+        lbl3.Data = lbl3Design;
+
+        Design[] labels = { lbl1Design, lbl2Design, lbl3Design };
+
+        d.View.Add( lbl1 );
+        d.View.Add( lbl2 );
+        d.View.Add( lbl3 );
+
+        SelectionManager selection = SelectionManager.Instance;
+        selection.Clear( );
+        MouseManager mgr = new( );
+
+        // user presses down
+        MouseEvent e = new( )
+        {
+            X = xStart,
+            Y = yStart,
+            Flags = MouseFlags.Button1Pressed
+        };
+
+        mgr.HandleMouse( e, d );
+
+        // user pulled selection box to destination
+        e = new( )
+        {
+            X = xEnd,
+            Y = yEnd,
+            Flags = MouseFlags.Button1Pressed
+        };
+        mgr.HandleMouse( e, d );
+
+        // user releases mouse (in place)
+        e = new( )
+        {
+            X = xEnd,
+            Y = yEnd
+        };
+        mgr.HandleMouse( e, d );
+
+        // do not expect dragging selection box to change anything
+        // or be undoable
+        Assert.That( OperationManager.Instance.UndoStackSize, Is.Zero );
+
+        // for each selectable thing
+        for ( int i = 0; i < labels.Length; i++ )
+        {
+            // its selection status should match the expectation
+            // passed in the test case
+            Assert.That(
+                selection.Selected.ToList( ).Contains( labels[ i ] ), Is.EqualTo( expectSelected.Contains( i ) ),
+                $"Expectation wrong for label index {i} (indexes are 0 based)" );
+        }
     }
 
     [Test]
@@ -306,142 +441,6 @@ internal class MouseManagerTests : Tests
             case (> 0, > 0): // Down and right
                 Assert.That( OperationManager.Instance.UndoStackSize, Is.EqualTo( 1 ) );
                 break;
-        }
-    }
-
-    [Test]
-    public void DragResizeView_CannotResize_1By1View( [Values( 0, 3 )] int locationOfViewX, [Values( 0, 3 )] int locationOfViewY )
-    {
-        Design d = Get10By10View( );
-
-        View view = ViewFactory.Create<View>( );
-        view.Width = Dim.Fill( );
-        view.X = locationOfViewX;
-        view.Y = locationOfViewY;
-        view.Width = 1;
-        view.Height = 1;
-
-        Design design = new( d.SourceCode, "myView", view );
-        view.Data = design;
-        d.View.Add( view );
-
-        d.View.LayoutSubviews( );
-
-        MouseManager mgr = new( );
-
-        // we haven't done anything yet
-        Assert.That( OperationManager.Instance.UndoStackSize, Is.Zero );
-        Assert.That( view.Frame.X, Is.EqualTo( locationOfViewX ) );
-        Assert.That( view.Frame.Y, Is.EqualTo( locationOfViewY ) );
-        Assert.That( view.Bounds.Width, Is.EqualTo( 1 ) );
-        Assert.That( view.Bounds.Height, Is.EqualTo( 1 ) );
-
-        // user presses down in the lower right of control
-        MouseEvent e = new( )
-        {
-            X = locationOfViewX,
-            Y = locationOfViewY,
-            Flags = MouseFlags.Button1Pressed
-        };
-
-        View? hit = view.HitTest( e, out bool isBorder, out bool isLowerRight );
-        Assert.That( hit, Is.SameAs( view ) );
-        Assert.That( isBorder );
-        Assert.That( isLowerRight, Is.False, "Upper left should never be considered lower right even if view is 1x1" );
-
-        mgr.HandleMouse( e, d );
-
-        Assert.That( view.Frame.Y, Is.EqualTo( locationOfViewY ) );
-
-        // we still haven't committed to anything
-        Assert.That( OperationManager.Instance.UndoStackSize, Is.Zero );
-
-        // user pulled view size down and left
-        e = new( )
-        {
-            X = 6,
-            Y = 3,
-            Flags = MouseFlags.Button1Pressed
-        };
-        mgr.HandleMouse( e, d );
-
-        Assert.That( view.Bounds.Width, Is.EqualTo( 1 ) );
-        Assert.That( view.Bounds.Height, Is.EqualTo( 1 ) );
-    }
-
-    [TestCase( 1, 1, 4, 6, new[] { 0, 2 } )]      // drag from 1,1 to 4,6 and expect labels 0 and 2 to be selected
-    [TestCase( 1, 1, 10, 10, new[] { 0, 1, 2 } )] // drag over all
-    public void DragSelectionBox( int xStart, int yStart, int xEnd, int yEnd, int[] expectSelected )
-    {
-        Design d = Get10By10View( );
-
-        /*
-          Hi
-            Hi
-          Hi
-        */
-
-        Label lbl1 = new( 2, 1, "Hi" );
-        Label lbl2 = new( 4, 2, "Hi" );
-        Label lbl3 = new( 2, 3, "Hi" );
-
-        Design lbl1Design = new( d.SourceCode, "lbl1", lbl1 );
-        Design lbl2Design = new( d.SourceCode, "lbl2", lbl2 );
-        Design lbl3Design = new( d.SourceCode, "lbl3", lbl3 );
-
-        lbl1.Data = lbl1Design;
-        lbl2.Data = lbl2Design;
-        lbl3.Data = lbl3Design;
-
-        Design[] labels = { lbl1Design, lbl2Design, lbl3Design };
-
-        d.View.Add( lbl1 );
-        d.View.Add( lbl2 );
-        d.View.Add( lbl3 );
-
-        SelectionManager selection = SelectionManager.Instance;
-        selection.Clear( );
-        MouseManager mgr = new( );
-
-        // user presses down
-        MouseEvent e = new( )
-        {
-            X = xStart,
-            Y = yStart,
-            Flags = MouseFlags.Button1Pressed
-        };
-
-        mgr.HandleMouse( e, d );
-
-        // user pulled selection box to destination
-        e = new( )
-        {
-            X = xEnd,
-            Y = yEnd,
-            Flags = MouseFlags.Button1Pressed
-        };
-        mgr.HandleMouse( e, d );
-
-        // user releases mouse (in place)
-        e = new( )
-        {
-            X = xEnd,
-            Y = yEnd
-        };
-        mgr.HandleMouse( e, d );
-
-        // do not expect dragging selection box to change anything
-        // or be undoable
-        Assert.That( OperationManager.Instance.UndoStackSize, Is.Zero );
-
-        // for each selectable thing
-        for ( int i = 0; i < labels.Length; i++ )
-        {
-            // its selection status should match the expectation
-            // passed in the test case
-            Assert.That(
-                selection.Selected.ToList( ).Contains( labels[ i ] ), Is.EqualTo( expectSelected.Contains( i ) ),
-                $"Expectation wrong for label index {i} (indexes are 0 based)" );
         }
     }
 
