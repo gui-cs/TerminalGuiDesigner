ba5aa8d5788486a9f11dfec6012e0ec0c1718882
Brandon Thetford <github@snapsinazfs.com>
2024-01-03 05:08:33 -0700

    Convert these

--- tests/RadioGroupTests.cs
+++ tests/RadioGroupTests.cs
@@ -8,41 +8,39 @@ namespace UnitTests;
 internal class RadioGroupTests : Tests
 {
     [Test]
-    public void RoundTrip_PreserveRadioGroups()
+    public void RoundTrip_PreserveRadioGroups( )
     {
+        var rgIn = RoundTrip<Window, RadioGroup>( static ( _, _ ) => { }, out _ );
 
-        var rgIn = RoundTrip<Window, RadioGroup>( static (_, _) => { }, out _);
+        Assert.That( rgIn.RadioLabels, Has.Length.EqualTo( 2 ) );
 
-        ClassicAssert.AreEqual(2, rgIn.RadioLabels.Length);
-
-        ClassicAssert.AreEqual("Option 1", rgIn.RadioLabels[0]);
-        ClassicAssert.AreEqual("Option 2", rgIn.RadioLabels[1]);
+        Assert.Multiple( ( ) =>
+        {
+            Assert.That( rgIn.RadioLabels[ 0 ], Is.EqualTo( "Option 1" ) );
+            Assert.That( rgIn.RadioLabels[ 1 ], Is.EqualTo( "Option 2" ) );
+        } );
     }
 
     [Test]
-    public void RoundTrip_PreserveRadioGroups_Custom()
+    public void RoundTrip_PreserveRadioGroups_Custom( )
     {
+        var rgIn = RoundTrip<Window, RadioGroup>( static ( _, r ) => { r.RadioLabels = ["Fish", "Cat", "Balloon"]; }, out _ );
 
-        var rgIn = RoundTrip<Window, RadioGroup>( static (_, r) =>
-            {
-                r.RadioLabels = [ "Fish", "Cat", "Balloon" ];
-        }, out _);
+        Assert.That( rgIn.RadioLabels, Has.Length.EqualTo( 3 ) );
 
-        ClassicAssert.AreEqual(3, rgIn.RadioLabels.Length);
-
-        ClassicAssert.AreEqual("Fish", rgIn.RadioLabels[0]);
-        ClassicAssert.AreEqual("Cat", rgIn.RadioLabels[1]);
-        ClassicAssert.AreEqual("Balloon", rgIn.RadioLabels[2]);
+        Assert.Multiple( ( ) =>
+        {
+            Assert.That( rgIn.RadioLabels[ 0 ], Is.EqualTo( "Fish" ) );
+            Assert.That( rgIn.RadioLabels[ 1 ], Is.EqualTo( "Cat" ) );
+            Assert.That( rgIn.RadioLabels[ 2 ], Is.EqualTo( "Balloon" ) );
+        } );
     }
 
     [Test]
-    public void RoundTrip_PreserveRadioGroups_Empty()
+    public void RoundTrip_PreserveRadioGroups_Empty( )
     {
-        var rgIn = RoundTrip<Window, RadioGroup>( static (_, r) =>
-            {
-                r.RadioLabels = [ ];
-            }, out _);
+        var rgIn = RoundTrip<Window, RadioGroup>( static ( _, r ) => { r.RadioLabels = []; }, out _ );
 
-        ClassicAssert.IsEmpty(rgIn.RadioLabels);
+        Assert.That( rgIn.RadioLabels, Is.Empty );
     }
 }
