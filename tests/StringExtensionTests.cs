a913d7a68d7bc652f608b64dee408bf4f6908184
Brandon Thetford <github@snapsinazfs.com>
2024-01-03 16:38:45 -0700

    Convert to constraints

--- tests/StringExtensionTests.cs
+++ tests/StringExtensionTests.cs
@@ -14,26 +14,26 @@ internal class StringExtensionTests
     [TestCase("  ", "blank", new[] { "fish1", "fish2" })]
     [TestCase("bob", "bob3", new[] { "bob", "bob2" })]
     [TestCase("Fish1", "Fish1", new[] { "fish1"})] // default behavior is case sensitive
-    public void TestMakeUnique_DefaultComparer(string? stringIn, string expectedOut, string[] whenCollection)
+    public void MakeUnique_DefaultComparer(string? stringIn, string expectedOut, string[] whenCollection)
     {
-        ClassicAssert.AreEqual(expectedOut, stringIn.MakeUnique(whenCollection));
+        Assert.That( stringIn.MakeUnique(whenCollection), Is.EqualTo( expectedOut ) );
     }
 
     [TestCase("bob", "bob", new[] { "fish" })]
     [TestCase("Fish", "Fish2", new[] { "fish" })]
     [TestCase("fIsh2", "fIsh3", new[] { "fish1", "fish2" })]
     [TestCase("fish2", "fish3", new[] { "fiSH1", "fiSH2" })]
-    public void TestMakeUnique_IgnoreCaps(string stringIn, string expectedOut, string[] whenCollection)
+    public void MakeUnique_IgnoreCaps(string stringIn, string expectedOut, string[] whenCollection)
     {
-        ClassicAssert.AreEqual(expectedOut, stringIn.MakeUnique(whenCollection,System.StringComparer.InvariantCultureIgnoreCase));
+        Assert.That( stringIn.MakeUnique(whenCollection,System.StringComparer.InvariantCultureIgnoreCase), Is.EqualTo( expectedOut ) );
     }
 
     [TestCase("a",4," a  ")]
     [TestCase("a", 5, "  a  ")]
     [TestCase("b l", 5, " b l ")]
     [TestCase("sooooLooong", 2, "sooooLooong")]
-    public void TestPadBoth(string input, int length, string expectedOutput)
+    public void PadBoth(string input, int length, string expectedOutput)
     {
-        ClassicAssert.AreEqual(expectedOutput, input.PadBoth(length));
+        Assert.That( input.PadBoth(length), Is.EqualTo( expectedOutput ) );
     }
 }
