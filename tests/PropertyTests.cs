b463a74f830e66ec4096fc5fe10774a5458ebcb2
Brandon Thetford <github@snapsinazfs.com>
2024-01-03 01:33:32 -0700

    Use the types in preparation for test updates
    Also clean up redundancies

--- tests/PropertyTests.cs
+++ tests/PropertyTests.cs
@@ -13,121 +13,121 @@ internal class PropertyTests : Tests
     [Test]
     public void PropertyOfType_Pos( )
     {
-        var d = new Design(new SourceCodeFile(nameof(PropertyOfType_Pos) + ".cs"), "FFF", new Label());
-        var xProp = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals(nameof(View.X)));
+        Design d = new(new(nameof(PropertyOfType_Pos) + ".cs"), "FFF", new Label());
+        Property xProp = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals(nameof(View.X)));
 
-        xProp.SetValue(Pos.Center());
+        xProp.SetValue( Pos.Center( ) );
 
-        var rhs = (CodeSnippetExpression)xProp.GetRhs();
+        CodeSnippetExpression rhs = (CodeSnippetExpression)xProp.GetRhs();
 
         // The code generated for a Property of Type Pos should be the function call
-        ClassicAssert.AreEqual(rhs.Value, "Pos.Center()");
+        ClassicAssert.AreEqual( rhs.Value, "Pos.Center()" );
     }
 
     [Test]
-    public void TestPropertyOfType_Size()
+    public void PropertyOfType_Size( )
     {
-        var d = new Design(new SourceCodeFile(nameof(PropertyOfType_Size) + ".cs"), "FFF", new ScrollView());
-        var xProp = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals(nameof(View.X)));
+        Design d = new(new(nameof(PropertyOfType_Size) + ".cs"), "FFF", new ScrollView());
+        Property xProp = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals(nameof(View.X)));
 
-        xProp.SetValue(Pos.Center());
+        xProp.SetValue( Pos.Center( ) );
 
-        var rhs = (CodeSnippetExpression)xProp.GetRhs();
+        CodeSnippetExpression rhs = (CodeSnippetExpression)xProp.GetRhs();
 
         // The code generated for a Property of Type Pos should be the function call
-        ClassicAssert.AreEqual(rhs.Value, "Pos.Center()");
+        ClassicAssert.AreEqual( rhs.Value, "Pos.Center()" );
     }
 
     [Test]
     public void PropertyOfType_Attribute( )
     {
-        var d = new Design(new SourceCodeFile(nameof(PropertyOfType_Attribute) + ".cs"), "FFF", new GraphView());
-        var colorProp = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals(nameof(GraphView.GraphColor)));
+        Design d = new(new(nameof(PropertyOfType_Attribute) + ".cs"), "FFF", new GraphView());
+        Property colorProp = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals(nameof(GraphView.GraphColor)));
 
-        colorProp.SetValue(null);
+        colorProp.SetValue( null );
 
-        var rhs = (CodeSnippetExpression)colorProp.GetRhs();
-        ClassicAssert.AreEqual(rhs.Value, "null");
+        CodeSnippetExpression rhs = (CodeSnippetExpression)colorProp.GetRhs();
+        ClassicAssert.AreEqual( rhs.Value, "null" );
 
-        colorProp.SetValue(new Attribute(Color.BrightMagenta, Color.Blue));
+        colorProp.SetValue( new Attribute( Color.BrightMagenta, Color.Blue ) );
 
-        rhs = (CodeSnippetExpression)colorProp.GetRhs();
-        ClassicAssert.AreEqual(rhs.Value, "new Terminal.Gui.Attribute(Color.BrightMagenta,Color.Blue)");
+        rhs = (CodeSnippetExpression)colorProp.GetRhs( );
+        ClassicAssert.AreEqual( rhs.Value, "new Terminal.Gui.Attribute(Color.BrightMagenta,Color.Blue)" );
     }
 
     [Test]
     public void PropertyOfType_PointF( )
     {
-        var d = new Design(new SourceCodeFile(nameof(PropertyOfType_PointF) + ".cs"), "FFF", new GraphView());
-        var pointProp = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals(nameof(GraphView.ScrollOffset)));
+        Design d = new(new(nameof(PropertyOfType_PointF) + ".cs"), "FFF", new GraphView());
+        Property pointProp = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals(nameof(GraphView.ScrollOffset)));
 
-        pointProp.SetValue(new PointF(4.5f, 4.1f));
+        pointProp.SetValue( new PointF( 4.5f, 4.1f ) );
 
-        var rhs = (CodeObjectCreateExpression)pointProp.GetRhs();
+        CodeObjectCreateExpression rhs = (CodeObjectCreateExpression)pointProp.GetRhs();
 
         // The code generated should be a new PointF
-        ClassicAssert.AreEqual(rhs.Parameters.Count, 2);
+        ClassicAssert.AreEqual( rhs.Parameters.Count, 2 );
     }
 
     [Test]
     public void PropertyOfType_Rune( )
     {
-        var viewToCode = new ViewToCode();
+        ViewToCode viewToCode = new();
 
-        var file = new FileInfo("TestPropertyOfType_Rune.cs");
-        var lv = new LineView();
-        var d = new Design(new SourceCodeFile(file), "lv", lv);
-        var prop = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals("LineRune"));
+        FileInfo file = new("TestPropertyOfType_Rune.cs");
+        LineView lv = new();
+        Design d = new(new(file), "lv", lv);
+        Property prop = d.GetDesignableProperties().Single( static p => p.PropertyInfo.Name.Equals("LineRune"));
 
-        prop.SetValue('F');
+        prop.SetValue( 'F' );
 
-        ClassicAssert.AreEqual(new Rune('F'), lv.LineRune);
+        ClassicAssert.AreEqual( new Rune( 'F' ), lv.LineRune );
 
-        var code = ExpressionToCode(prop.GetRhs());
+        string code = ExpressionToCode(prop.GetRhs());
 
-        ClassicAssert.AreEqual("new System.Text.Rune('F')", code);
+        ClassicAssert.AreEqual( "new System.Text.Rune('F')", code );
     }
 
     [Test]
     public void Changing_LineViewOrientation( )
     {
-        var v = Get10By10View();
-        var lv = (LineView)ViewFactory.Create(typeof(LineView));
-        var d = new Design(v.SourceCode, "lv", lv);
+        Design v = Get10By10View();
+        LineView lv = (LineView)ViewFactory.Create(typeof(LineView));
+        Design d = new(v.SourceCode, "lv", lv);
 
-        v.View.Add(lv);
+        v.View.Add( lv );
         lv.IsInitialized = true;
 
-        ClassicAssert.AreEqual(Orientation.Horizontal, lv.Orientation);
-        ClassicAssert.AreEqual(new Rune('─'), lv.LineRune);
-        var prop = d.GetDesignableProperty(nameof(LineView.Orientation));
+        ClassicAssert.AreEqual( Orientation.Horizontal, lv.Orientation );
+        ClassicAssert.AreEqual( new Rune( '─' ), lv.LineRune );
+        Property? prop = d.GetDesignableProperty(nameof(LineView.Orientation));
 
-        ClassicAssert.IsNotNull(prop);
-        prop?.SetValue(Orientation.Vertical);
-        ClassicAssert.AreEqual(ConfigurationManager.Glyphs.VLine, lv.LineRune);
+        ClassicAssert.IsNotNull( prop );
+        prop?.SetValue( Orientation.Vertical );
+        ClassicAssert.AreEqual( ConfigurationManager.Glyphs.VLine, lv.LineRune );
 
         // now try with a dim fill
-        lv.Height = Dim.Fill();
+        lv.Height = Dim.Fill( );
         lv.Width = 1;
 
-        prop?.SetValue(Orientation.Horizontal);
-        ClassicAssert.AreEqual(Orientation.Horizontal, lv.Orientation);
-        ClassicAssert.AreEqual(ConfigurationManager.Glyphs.HLine, lv.LineRune);
-        ClassicAssert.AreEqual(Dim.Fill(), lv.Width);
-        ClassicAssert.AreEqual(Dim.Sized(1), lv.Height);
+        prop?.SetValue( Orientation.Horizontal );
+        ClassicAssert.AreEqual( Orientation.Horizontal, lv.Orientation );
+        ClassicAssert.AreEqual( ConfigurationManager.Glyphs.HLine, lv.LineRune );
+        ClassicAssert.AreEqual( Dim.Fill( ), lv.Width );
+        ClassicAssert.AreEqual( Dim.Sized( 1 ), lv.Height );
     }
 
     public static string ExpressionToCode( CodeExpression expression )
     {
         CSharpCodeProvider provider = new ();
 
-        using (var sw = new StringWriter())
+        using ( StringWriter sw = new( ) )
         {
-            IndentedTextWriter tw = new IndentedTextWriter(sw, "    ");
-            provider.GenerateCodeFromExpression(expression, tw, new CodeGeneratorOptions());
-            tw.Close();
+            IndentedTextWriter tw = new(sw, "    ");
+            provider.GenerateCodeFromExpression( expression, tw, new( ) );
+            tw.Close( );
 
-            return sw.GetStringBuilder().ToString();
+            return sw.GetStringBuilder( ).ToString( );
         }
     }
 }
