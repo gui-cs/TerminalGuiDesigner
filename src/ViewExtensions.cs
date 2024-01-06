75930446c87ca83d0623bbd70b41d6b2e20bcfe4
Brandon Thetford <github@snapsinazfs.com>
2024-01-06 02:27:16 -0700

    Fix bug making it impossible for isLowerRight to be true.
    May need additional scrutiny for any special cases not covered by other existing logic

--- src/ViewExtensions.cs
+++ src/ViewExtensions.cs
@@ -267,7 +267,7 @@ public static class ViewExtensions
             hit = hit.SuperView;
         }
 
-        int resizeBoxArea = 2;
+        const int resizeBoxArea = 2;
 
         if (hit != null)
         {
@@ -275,8 +275,12 @@ public static class ViewExtensions
 
             if (point != new Point(screenFrame.X, screenFrame.Y))
             {
-                isLowerRight = Math.Abs(screenFrame.X + screenFrame.Width - point.X) <= resizeBoxArea
-                && Math.Abs(screenFrame.Y + screenFrame.Height - point.Y) <= resizeBoxArea;
+                ref int xClickCoordinateInView = ref point.X;
+                ref int yClickCoordinateInView = ref point.Y;
+                int xDistanceFromBottomRight = screenFrame.Width - xClickCoordinateInView - 1;
+                int yDistanceFromBottomRight = screenFrame.Height - yClickCoordinateInView - 1;
+
+                isLowerRight = xDistanceFromBottomRight <= resizeBoxArea && yDistanceFromBottomRight <= resizeBoxArea;
             }
             else
             {
