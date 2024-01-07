164f6263379d96a7057f4d2af3d9c4774798bdbc
tznind <tznind@dundee.ac.uk>
2024-01-07 09:26:06 +0000

    Add Investigation test for menu bars

---
+++ tests/Investigations/Investigation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerminalGuiDesigner.UI;
+using YamlDotNet.Core.Tokens;
+
+namespace UnitTests.Investigations
+{
+    abstract class Investigation
+    {
+        protected abstract string Cs { get; }
+        protected abstract string DesignerCs { get; }
+
+        protected abstract void MakeAssertions(Design rootDesign);
+
+
+        [Test]
+        public void PerformTest()
+        {
+            var designerFile = new FileInfo(
+                Path.Combine(
+                TestContext.CurrentContext.WorkDirectory, this.GetType().Name + ".Designer.cs")
+                );
+
+            File.WriteAllText(designerFile.FullName, DesignerCs);
+
+            var csFile = new FileInfo(
+                Path.Combine(
+                TestContext.CurrentContext.WorkDirectory, this.GetType().Name + ".cs")
+                );
+
+            File.WriteAllText(csFile.FullName, DesignerCs);
+
+            var decompiler = new CodeToView(new SourceCodeFile(designerFile));
+            var d = decompiler.CreateInstance();
+            MakeAssertions(d);
+        }
+    }
+}
