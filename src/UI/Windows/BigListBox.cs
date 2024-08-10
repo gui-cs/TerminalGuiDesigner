769074ef8e2670b7cd3c63622001826feb13e61f
tznind <tznind@dundee.ac.uk>
2024-08-10 16:36:46 +0100

    As recommended switch to NewKeyDownEvent

--- src/UI/Windows/BigListBox.cs
+++ src/UI/Windows/BigListBox.cs
@@ -222,7 +222,7 @@ public class BigListBox<T>
         if (key == Key.Backspace || char.IsLetterOrDigit(c))
         {
             this.searchBox?.FocusFirst(TabBehavior.TabStop);
-            this.searchBox?.OnProcessKeyDown(key);
+            this.searchBox?.NewKeyDownEvent(key);
             key.Handled = true;
         }
     }
