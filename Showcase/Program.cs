7f18bd31e4fa9856da5b1d12a7bce668fd556e37
tznind <tznind@dundee.ac.uk>
2024-09-01 10:49:15 +0100

    Add Showcase and implement for NumericUpDown

---
+++ Showcase/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.InteropServices.ComTypes;
+using Terminal.Gui;
+
+namespace Showcase
+{
+    internal class Program
+    {
+        private static Type[] views = new[]
+        {
+            typeof(NumericUpDown) 
+
+        };
+        static void Main(string[] args)
+        {
+            Application.Init();
+
+            var w = new Window()
+            {
+                Title = "Showcase"
+            };
+
+            var lv = new ListView()
+            {
+                Width = Dim.Fill(),
+                Height = Dim.Fill(),
+            };
+            w.Add(lv);
+            lv.SetSource(new ObservableCollection<Type>(views));
+            
+
+            lv.KeyDown += (_, e) =>
+            {
+                if (e.KeyCode == KeyCode.Enter)
+                {
+                    var v = (Toplevel)Activator.CreateInstance(views[lv.SelectedItem]);
+                    e.Handled = true;
+                    Application.Run(v);
+                }
+            };
+
+            Application.Run(w);
+            Application.Shutdown();
+        }
+    }
+}
